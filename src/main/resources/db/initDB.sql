016a7ddfea937a1d85d8405307ed028586b15c44
DrPatvar <Shurkoff@gmail.com>
2023-02-17 17:31:45 +0300

    HW03_homework

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE global_seq_meal START WITH 100;
 
 CREATE TABLE users
 (
@@ -23,3 +25,15 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+    dateTime    TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    user_id     INTEGER   NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_dateTime ON meals (dateTime);
+CREATE INDEX meals_user_id ON meals (user_id);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_role;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meal RESTART WITH 100;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -10,3 +12,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2023-02-17 08:30:05', 'BreakFast', 500, 100000),
+       ('2023-02-17 12:25:05','Lunch', 500, 100000),
+       ('2023-02-17 18:30:05','Dinner', 700, 100000),
+       ('2023-02-16 08:15:17', 'Breakfast', 350,100000),
+       ('2023-02-16 12:00:05','Lunch',1350,100000),
+       ('2023-02-16 19:11:11', 'Dinner', 750, 100000);
